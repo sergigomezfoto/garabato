2a447bd3d5536f0fd8c338fdb1df3ef5b31d0241
Oriol Rocabert <oriol.rocabert.valverde@gmail.com>
2023-08-29 09:55:25 +0200

    Fixed layout and added it to pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,12 +9,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<>
-			<html lang="en">
+		<html lang="en">
+			<body className={inter.className}>
 				<header>Header</header>
-				<body className={inter.className}>{children}</body>
+				{children}
 				<footer>Footer</footer>
-			</html>
-		</>
+			</body>
+		</html>
 	);
 }
