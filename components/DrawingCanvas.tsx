fd8e371baaf25148a5f2aa36a26f13d0a35d9a7d
Oriol Rocabert <oriol.rocabert.valverde@gmail.com>
2023-08-29 09:40:21 +0200

    Basic layout

--- components/DrawingCanvas.tsx
+++ components/DrawingCanvas.tsx
@@ -1,13 +1,13 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import {ReactSketchCanvas} from 'react-sketch-canvas';
+"use client";
+import React, { useState, useEffect } from "react";
+import { ReactSketchCanvas } from "react-sketch-canvas";
 
 export default function DrawingCanvas() {
-  const [width, setWidth] = useState("30%");
-  const [height, setHeight] = useState("40%");
+	const [width, setWidth] = useState("30%");
+	const [height, setHeight] = useState("40%");
 
-  //Effect to handle windows resize, prob not needed for mobile
-  /* useEffect(() => {
+	//Effect to handle windows resize, prob not needed for mobile
+	/* useEffect(() => {
     const handleResize = () => {
       setWidth(window.innerWidth);
       setHeight(window.innerHeight);
@@ -20,12 +20,12 @@ export default function DrawingCanvas() {
     };
   }, []); */
 
-  return (
-    <ReactSketchCanvas
-      width={`{width}`}
-      height={`{height}`}
-      strokeWidth={5}
-      strokeColor='red'
-    />
-  );
+	return (
+		<ReactSketchCanvas
+			width={`{width}`}
+			height={`{height}`}
+			strokeWidth={5}
+			strokeColor="red"
+		/>
+	);
 }
