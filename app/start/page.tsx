da9579a113cb7adfcd658a22dba1f89cbaec4dc9
sergigomezfoto <sergi@sergigomez.com>
2023-09-04 11:03:54 +0200

    comit final firebase working

---
+++ app/start/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import DrawingCanvas from '@/components/DrawingCanvas';
+
+export default function UserID() {
+
+
+  return (
+    <div>
+      <h2>frase a dibujar</h2>
+      <DrawingCanvas/>
+    </div>
+  );
+}
