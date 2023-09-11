dc9435247dbed60598452ee51e106d88df3f5ecc
Pablo Mena Rodriguez <pablo.mena.rodriguez@hp.com>
2023-09-11 11:34:37 +0200

    create or join, home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import UserID from "@/components/UserID";
+import Link from 'next/link';
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <UserID/>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl mb-4">Welcome to the game, choose an option</h1>
+      <div>
+        <Link href="/create">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
+            Create
+          </button>
+        </Link>
+        <Link href="/join">
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-2">
+            Join
+          </button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default HomePage;
