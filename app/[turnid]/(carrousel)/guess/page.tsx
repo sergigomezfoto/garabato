eebc4f5907f68498b9d408cd2a2cd397e3d05205
Oriol Rocabert <oriol.rocabert.valverde@gmail.com>
2023-09-17 16:28:51 +0200

    "guess" page retrieves from DB. It stores individual guesses Removed doneButton functionality

--- app/[turnid]/(carrousel)/guess/page.tsx
+++ app/[turnid]/(carrousel)/guess/page.tsx
@@ -1,58 +1,101 @@
 "use client";
 
-import { DoneButtonStructure } from "@/components/DoneButton";
 import { db } from "@/firebase/firebase";
-import { collection, onSnapshot, doc } from "firebase/firestore";
+import { collection, doc, getDocs, setDoc } from "firebase/firestore";
+import { useParams } from "next/navigation";
 import { useState, useEffect } from "react";
 
 const ShowDrawing = () => {
+	//TODO
+	//Room name comes from before
 	const sala = "hola";
-
-	const [players, setPlayers] = useState<any[]>([]);
-
-	useEffect(() => {
-		const playersCollectionRef = collection(db, "grabatoTest", sala, "players");
-		const unsubscribePlayers = onSnapshot(playersCollectionRef, (snapshot) => {
-			const playersData = snapshot.docs.map((doc) => doc.data());
-			setPlayers(playersData);
-		});
-
-		return () => unsubscribePlayers(); // Desubscriure's quan el component es desmonti
-	}, [sala]);
-
-	return (
-		<div className="flex flex-wrap max-w-md justify-center gap-4">
-			{players.map((player, index) => (
-				<div key={index} className="flex flex-col items-center space-y-2">
-					<img
-						src={player.avatar}
-						alt={player.name}
-						className="rounded-full w-16 h-16"
-					/>
-					<span className="text-lg">{player.name}</span>
-				</div>
-			))}
-		</div>
-	);
-
-	/*	//This variables will be obtained from DB
-	const user = "Uri";
+	//Drawing comes from before, for now using random picture.
 	const image =
 		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfdJElDmsk5euD5idRSZMgBHYSPkI0ECTH8OmEm93E4PFQN5ZcLUuuDwedKrqpIYLTaE0&usqp=CAU";
 
-	return (
-		<div className="flex flex-col justify-center items-center">
-			<h1>Este es el dibujo de {user}.</h1>
-			<img src={image} alt="Dibujo" className="m-5" />
-			<h1>Descríbelo con pocas palabras.</h1>
-			<form>
-				<input className="m-3" type="text" placeholder="" />
-				<DoneButtonStructure text="¡Hecho!" hexCode="#FFB6C1" />
-			</form>
-		</div>
+	const [players, setPlayers] = useState<any[]>([]);
+	const { turnid } = useParams();
+	const turnidNumber = parseInt(turnid as string, 10);
+	const [guess, setGuess] = useState("");
+
+	useEffect(() => {
+		//Create reference to players in DB.
+		const playersCollectionRef = collection(db, "grabatoTest", sala, "players");
+		//Get all players information.
+		getDocs(playersCollectionRef)
+			.then((querySnapshot) => {
+				const playersData = querySnapshot.docs.map((doc) => {
+					// Include the document ID as a property
+					const dataWithId = {
+						playerID: doc.id,
+						playerFields: doc.data(),
+					};
+					return dataWithId;
+				});
+				setPlayers(playersData);
+			})
+			.catch((error) => {
+				console.error("Error fetching players:", error);
+			});
+	}, [sala]);
+
+	//Filter player based on turnidNumber.
+	const currentPlayer = players.find(
+		(player) => player.playerFields.turnid === turnidNumber
 	);
 
-	*/
+	//Handle form submit.
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		//Create reference to player in DB.
+		const playerDocRef = doc(
+			db,
+			"grabatoTest",
+			sala,
+			"players",
+			currentPlayer.playerID
+		);
+
+		try {
+			//Update the player's document with the guess
+			await setDoc(playerDocRef, { guess }, { merge: true });
+			console.log("Guess saved successfully!");
+		} catch (error) {
+			console.error("Error saving guess:", error);
+		}
+	};
+
+	return (
+		<div className="flex flex-col justify-center items-center">
+			{currentPlayer ? (
+				<div className="flex flex-col items-center space-y-2">
+					<h1>Este es el dibujo de {currentPlayer.name}.</h1>
+					<img src={image} alt="Dibujo" className="m-5" />
+					<h1>Descríbelo con pocas palabras.</h1>
+					<form onSubmit={handleSubmit}>
+						<input
+							className="m-3"
+							type="text"
+							placeholder=""
+							name="pictureGuess"
+							value={guess}
+							onChange={(e) => setGuess(e.target.value)}
+						/>
+						<button
+							type="submit"
+							className="rounded-full shadow-lg px-1 py-0.5"
+							style={{ backgroundColor: "#FFB6C1" }}
+						>
+							¡Hecho!
+						</button>
+					</form>
+				</div>
+			) : (
+				<p>No matching player found for turnid: {turnid}</p>
+			)}
+		</div>
+	);
 };
 
 export default ShowDrawing;
