b7221382f9c8aa78696d8ad1b5afeeb3c5ea5ab9
Celeste Ortiz <maria.celeste.ortz@gmail.com>
2023-09-21 18:55:37 +0200

    Conexión a la página guess una vez todos los jugadores han enviado su dibujo

--- app/(gamepreparation)/drawing/page.tsx
+++ app/(gamepreparation)/drawing/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import React, { useEffect, useRef, useState } from 'react';
-import { doc, updateDoc, getDoc } from 'firebase/firestore';
+import { doc, updateDoc, getDoc, collection, onSnapshot, getDocs } from 'firebase/firestore';
 import { db } from "@/firebase/firebase";
 import { ReactSketchCanvasRef } from 'react-sketch-canvas';
 import DrawingCanvas from '@/components/DrawingCanvas';
 import ButtonPromise from '../../../components/design/ButtonPromise';
-
+import { useRouter } from 'next/navigation';
+import ProgressBar from '@/components/ProgressBar';
 
 type StoredPlayerData = {
   playerId: string;
@@ -26,9 +27,14 @@ const Drawing = () => {
 
   const [player, setPlayer] = useState<StoredPlayerData>({ playerId: "", sala: "" });
   const [playerData, setPlayerData] = useState<PlayerData | undefined>(undefined);
+  const [playersDrawDone, setPlayersDrawDone] = useState<PlayerData[]>([]);
+  const [players, setPlayers] = useState<PlayerData[]>([]);
   const canvasRef = useRef<ReactSketchCanvasRef | null>(null);
+  const [draw, setDraw] = useState<boolean>(false);
+  const router = useRouter();
 
   useEffect(() => {
+    loadPlayers();
     const storedData = localStorage.getItem('GarabatoTest');
     if (storedData) {
       const parsedData = JSON.parse(storedData);
@@ -45,55 +51,76 @@ const Drawing = () => {
         };
         fetchData();
       }
-    }
-  }, []);
+      const playersCollectionRef = collection(db, "grabatoTest", parsedData.sala, "players");
+      const unsubscribePlayers = onSnapshot(playersCollectionRef, (snapshot) => {
+        const playersDone = snapshot.docs
+          .map((doc) => doc.data().drawing)
+          .filter((value) => value !== null);
+        setPlayersDrawDone(playersDone);
+      });
+      if (draw === true && players.length === playersDrawDone.length) {
+        router.push('/0/guess');
+      }
 
-  // const handleOnClick = async (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   console.log(player);
-  //   canvasRef.current!
-  //     .exportImage('png')
-  //     .then(async draw => {
-  //       if (draw) {
-  //         //Update user drawing data
-  //         const playerRef = doc(db, "grabatoTest", player.sala, "players", player.playerId);
-  //         await updateDoc(playerRef, {
-  //           drawing: draw
-  //         }).then(() => {
-  //           console.log("Data saved successfully!");
-  //         })
-  //           .catch(error => {
-  //             throw error;
-  //           })
-  //       }
-  //     }
-  //     )
-  // }
+      return () => unsubscribePlayers();
+    }
+  }, [playersDrawDone]);
+
+  const loadPlayers = () => {
+    const storedData = localStorage.getItem('GarabatoTest');
+    if (storedData) {
+        const data = JSON.parse(storedData);
+        if(data){
+        const playersCollectionRef = collection(db, 'grabatoTest', data.sala, 'players');
+        getDocs(playersCollectionRef).then(snapshot => {
+            const allPlayers: PlayerData[] = snapshot.docs.map(doc => ({
+                name: doc.data().name,
+                score: doc.data().score,
+                avatar: doc.data().avatar,
+                drawing: doc.data().drawing,
+                phrase: doc.data().phrase,
+                turnId: doc.data().turnId,
+            }));
+
+            setPlayers(allPlayers);
+        });
+    }}
+  }
+
   const handleOnClick = async () => {
     try {
       console.log(player);
-      const draw = await canvasRef.current!.exportImage('png');
-      if (draw) {
+      const drawing = await canvasRef.current!.exportImage('png');
+      if (drawing) {
         const playerRef = doc(db, "grabatoTest", player.sala, "players", player.playerId);
-        await updateDoc(playerRef, { drawing: draw });
+        await updateDoc(playerRef, { drawing: drawing });
         console.log("Data saved successfully!");
       }
     } catch (error) {
       console.error(error);
       throw error;
     }
-}
+    setDraw(true);
+  }
 
 
   return (
     <>
-      <h1 className="text-2xl my-6"><strong>{playerData?.phrase || 'cargando...'}</strong></h1>
-      <DrawingCanvas canvasRef={canvasRef} />
-      <i className="mb-4">¡Dibuja la frase que te ha tocado!</i>
-      <ButtonPromise onClick={handleOnClick}>
-        ¡Enviar dibujo!
-     </ButtonPromise>
+    {draw ?(<ProgressBar
+          totalPlayers={players.length}
+          playersReady={playersDrawDone.length}
+        />): (
+          <>
+            <h1 className="text-2xl my-6"><strong>{playerData?.phrase || 'cargando...'}</strong></h1>
+            <DrawingCanvas canvasRef={canvasRef} />
+            <i className="mb-4">¡Dibuja la frase que te ha tocado!</i>
+            <ButtonPromise onClick={handleOnClick}>
+              ¡Enviar dibujo!
+            </ButtonPromise>
+          </>)
+    }
     </>
+
 
   );
 }
