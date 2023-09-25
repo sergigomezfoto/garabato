8ad15aeafe59a04d27d83606f56e9b71ce99c4a2
Oriol Rocabert <oriol.rocabert.valverde@gmail.com>
2023-09-25 14:10:44 +0200

    Merged changes from main into modify-carrousel Fixed localStorage error with an if-else

--- app/[turnid]/(carrousel)/guess/page.tsx
+++ app/[turnid]/(carrousel)/guess/page.tsx
@@ -23,97 +23,110 @@ const GuessDrawing = () => {
 	const [guess, setGuess] = useState("");
 
 	const localStorageItem = localStorage.getItem("GarabatoTest");
-	const { playerId: myId, sala: sala } = JSON.parse(localStorageItem);
 
-	//Fetch all players data and set up listener
-	useEffect(() => {
-		fetchPlayersData(sala, setPlayers, myId, setMyTurn);
+	if (localStorageItem) {
+		const { playerId: myId, sala: sala } = JSON.parse(localStorageItem);
 
-		const playersCollectionRef = collection(db, "grabatoTest", sala, "players");
-		const unsubscribePlayers = onSnapshot(playersCollectionRef, (snapshot) => {
-			const playersDone = snapshot.docs
-				.map((doc) => doc.data().guessMade)
-				.filter((value) => value !== undefined);
-			setActionList(playersDone);
-		});
+		//Fetch all players data and set up listener
+		useEffect(() => {
+			fetchPlayersData(sala, setPlayers, myId, setMyTurn);
 
-		return () => unsubscribePlayers();
-	}, []);
-
-	//Filter player based on turnIdNumber.
-	useEffect(() => {
-		if (players) {
-			const currentPlayer = players.find(
-				(player: { playerFields: { turnId: number } }) =>
-					player.playerFields.turnId === turnIdNumber
+			const playersCollectionRef = collection(
+				db,
+				"grabatoTest",
+				sala,
+				"players"
+			);
+			const unsubscribePlayers = onSnapshot(
+				playersCollectionRef,
+				(snapshot) => {
+					const playersDone = snapshot.docs
+						.map((doc) => doc.data().guessMade)
+						.filter((value) => value !== undefined);
+					setActionList(playersDone);
+				}
 			);
 
-			if (currentPlayer) {
-				setCurrentPlayer(currentPlayer);
-				if (currentPlayer.playerFields.turnId === myTurn) {
-					setActionStatus(true);
+			return () => unsubscribePlayers();
+		}, []);
+
+		//Filter player based on turnIdNumber.
+		useEffect(() => {
+			if (players) {
+				const currentPlayer = players.find(
+					(player: { playerFields: { turnId: number } }) =>
+						player.playerFields.turnId === turnIdNumber
+				);
+
+				if (currentPlayer) {
+					setCurrentPlayer(currentPlayer);
+					if (currentPlayer.playerFields.turnId === myTurn) {
+						setActionStatus(true);
+					}
 				}
 			}
-		}
-	}, [players]);
+		}, [players]);
 
-	// Handle form submit
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		await handleUpdate(sala, myId, guess, "guessMade", setActionStatus);
-	};
+		// Handle form submit
+		const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
+			await handleUpdate(sala, myId, guess, "guessMade", setActionStatus);
+		};
 
-	//Reroute players when all players are done.
-	useEffect(() => {
-		if (actionStatus === true && players?.length === actionList?.length + 1) {
-			router.push(`/${turnIdNumber}/vote`);
-		}
-	}, [actionList, actionStatus]);
+		//Reroute players when all players are done.
+		useEffect(() => {
+			if (actionStatus === true && players?.length === actionList?.length + 1) {
+				router.push(`/${turnIdNumber}/vote`);
+			}
+		}, [actionList, actionStatus]);
 
-	return (
-		<div className="flex flex-col justify-center items-center">
-			{currentPlayer ? (
-				actionStatus === false ? (
-					<div className="flex flex-col items-center space-y-2">
-						<h1>Este es el dibujo de {currentPlayer.playerFields.name}.</h1>
-						<img
-							src={currentPlayer.playerFields.drawing}
-							alt="Dibujo"
-							className="m-5"
-						/>
-						<h1>Descríbelo con pocas palabras.</h1>
-						<form onSubmit={handleSubmit}>
-							<input
-								className="m-3"
-								type="text"
-								placeholder=""
-								name="pictureGuess"
-								value={guess}
-								onChange={(e) => setGuess(e.target.value)}
-								autoComplete="off"
-								required
+		return (
+			<div className="flex flex-col justify-center items-center">
+				{currentPlayer ? (
+					actionStatus === false ? (
+						<div className="flex flex-col items-center space-y-2">
+							<h1>Este es el dibujo de {currentPlayer.playerFields.name}.</h1>
+							<img
+								src={currentPlayer.playerFields.drawing}
+								alt="Dibujo"
+								className="m-5"
 							/>
-							<button
-								type="submit"
-								className="rounded-full shadow-lg px-1 py-0.5"
-								style={{ backgroundColor: "#FFB6C1" }}
-							>
-								¡Hecho!
-							</button>
-						</form>
-					</div>
+							<h1>Descríbelo con pocas palabras.</h1>
+							<form onSubmit={handleSubmit}>
+								<input
+									className="m-3"
+									type="text"
+									placeholder=""
+									name="pictureGuess"
+									value={guess}
+									onChange={(e) => setGuess(e.target.value)}
+									autoComplete="off"
+									required
+								/>
+								<button
+									type="submit"
+									className="rounded-full shadow-lg px-1 py-0.5"
+									style={{ backgroundColor: "#FFB6C1" }}
+								>
+									¡Hecho!
+								</button>
+							</form>
+						</div>
+					) : (
+						<ProgressBar
+							totalPlayers={players.length}
+							playersReady={actionList.length + 1}
+							text="Espera a que todos los jugadores envien su palabra."
+						/>
+					)
 				) : (
-					<ProgressBar
-						totalPlayers={players.length}
-						playersReady={actionList.length + 1}
-						text="Espera a que todos los jugadores envien su palabra."
-					/>
-				)
-			) : (
-				<p>No se ha encontrado ningun jugador con el turnid: {turnid}</p>
-			)}
-		</div>
-	);
+					<p>No se ha encontrado ningun jugador con el turnid: {turnid}</p>
+				)}
+			</div>
+		);
+	} else {
+		return <p>Hay un problema con recuperar los datos del local storage.</p>;
+	}
 };
 
 export default GuessDrawing;
--- app/[turnid]/(carrousel)/vote/page.tsx
+++ app/[turnid]/(carrousel)/vote/page.tsx
@@ -34,98 +34,111 @@ const VoteDrawing = () => {
 	const [vote, setVote] = useState("");
 
 	const localStorageItem = localStorage.getItem("GarabatoTest");
-	const { playerId: myId, sala: sala } = JSON.parse(localStorageItem);
 
-	//Fetch all players data and set up listener
-	useEffect(() => {
-		fetchPlayersData(sala, setPlayers, myId, setMyTurn);
+	if (localStorageItem) {
+		const { playerId: myId, sala: sala } = JSON.parse(localStorageItem);
 
-		const playersCollectionRef = collection(db, "grabatoTest", sala, "players");
-		const unsubscribePlayers = onSnapshot(playersCollectionRef, (snapshot) => {
-			const playersDone = snapshot.docs
-				.map((doc) => doc.data().guessVoted)
-				.filter((value) => value !== undefined);
-			setActionList(playersDone);
-		});
+		//Fetch all players data and set up listener
+		useEffect(() => {
+			fetchPlayersData(sala, setPlayers, myId, setMyTurn);
 
-		return () => unsubscribePlayers();
-	}, []);
-
-	//Filter player based on turnIdNumber.
-	useEffect(() => {
-		if (players) {
-			const currentPlayer = players.find(
-				(player: { playerFields: { turnId: number } }) =>
-					player.playerFields.turnId === turnIdNumber
+			const playersCollectionRef = collection(
+				db,
+				"grabatoTest",
+				sala,
+				"players"
+			);
+			const unsubscribePlayers = onSnapshot(
+				playersCollectionRef,
+				(snapshot) => {
+					const playersDone = snapshot.docs
+						.map((doc) => doc.data().guessVoted)
+						.filter((value) => value !== undefined);
+					setActionList(playersDone);
+				}
 			);
 
-			if (currentPlayer) {
-				setCurrentPlayer(currentPlayer);
-				if (currentPlayer.playerFields.turnId === myTurn) {
-					setActionStatus(true);
+			return () => unsubscribePlayers();
+		}, []);
+
+		//Filter player based on turnIdNumber.
+		useEffect(() => {
+			if (players) {
+				const currentPlayer = players.find(
+					(player: { playerFields: { turnId: number } }) =>
+						player.playerFields.turnId === turnIdNumber
+				);
+
+				if (currentPlayer) {
+					setCurrentPlayer(currentPlayer);
+					if (currentPlayer.playerFields.turnId === myTurn) {
+						setActionStatus(true);
+					}
 				}
 			}
-		}
-	}, [players]);
+		}, [players]);
 
-	// Handle vote
-	const handleVote = async (vote: string) => {
-		await handleUpdate(sala, myId, vote, "guessVoted", setActionStatus);
-	};
+		// Handle vote
+		const handleVote = async (vote: string) => {
+			await handleUpdate(sala, myId, vote, "guessVoted", setActionStatus);
+		};
 
-	//Reroute players when all players are done.
-	useEffect(() => {
-		if (actionStatus === true && players?.length === actionList?.length + 1) {
-			router.push(`/${turnIdNumber}/results`);
-		}
-	}, [actionList, actionStatus]);
+		//Reroute players when all players are done.
+		useEffect(() => {
+			if (actionStatus === true && players?.length === actionList?.length + 1) {
+				router.push(`/${turnIdNumber}/results`);
+			}
+		}, [actionList, actionStatus]);
 
-	return (
-		<div className="flex flex-col justify-center items-center">
-			{currentPlayer ? (
-				actionStatus === false ? (
-					<div className="flex flex-col items-center space-y-2">
-						<h1>Este es el dibujo de {currentPlayer.playerFields.name}.</h1>
-						<img
-							src={currentPlayer.playerFields.drawing}
-							alt="Dibujo"
-							className="m-5"
+		return (
+			<div className="flex flex-col justify-center items-center">
+				{currentPlayer ? (
+					actionStatus === false ? (
+						<div className="flex flex-col items-center space-y-2">
+							<h1>Este es el dibujo de {currentPlayer.playerFields.name}.</h1>
+							<img
+								src={currentPlayer.playerFields.drawing}
+								alt="Dibujo"
+								className="m-5"
+							/>
+							<h1>Vota lo que crees que es.</h1>
+
+							<ul className="flex flex-wrap justify-center items-center gap-4">
+								{players.map((player: Player, index: number) => (
+									<button
+										key={index}
+										value={vote}
+										className="p-2 bg-orange-500 m-1 rounded-lg text-white hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-300"
+										onClick={() =>
+											handleVote(
+												player.playerFields.turnId === turnIdNumber
+													? player.playerFields.phrase
+													: player.playerFields.guessMade
+											)
+										}
+									>
+										{player.playerFields.turnId === turnIdNumber
+											? player.playerFields.phrase
+											: player.playerFields.guessMade}
+									</button>
+								))}
+							</ul>
+						</div>
+					) : (
+						<ProgressBar
+							totalPlayers={players.length}
+							playersReady={actionList.length + 1}
+							text="Espera a que todos los jugadores voten."
 						/>
-						<h1>Vota lo que crees que es.</h1>
-
-						<ul className="flex flex-wrap justify-center items-center gap-4">
-							{players.map((player: Player, index: number) => (
-								<button
-									key={index}
-									value={vote}
-									className="p-2 bg-orange-500 m-1 rounded-lg text-white hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-300"
-									onClick={() =>
-										handleVote(
-											player.playerFields.turnId === turnIdNumber
-												? player.playerFields.phrase
-												: player.playerFields.guessMade
-										)
-									}
-								>
-									{player.playerFields.turnId === turnIdNumber
-										? player.playerFields.phrase
-										: player.playerFields.guessMade}
-								</button>
-							))}
-						</ul>
-					</div>
+					)
 				) : (
-					<ProgressBar
-						totalPlayers={players.length}
-						playersReady={actionList.length + 1}
-						text="Espera a que todos los jugadores voten."
-					/>
-				)
-			) : (
-				<p>No se ha encontrado ningun jugador con el turnid: {turnid}</p>
-			)}
-		</div>
-	);
+					<p>No se ha encontrado ningun jugador con el turnid: {turnid}</p>
+				)}
+			</div>
+		);
+	} else {
+		return <p>Hay un problema con recuperar los datos del local storage.</p>;
+	}
 };
 
 export default VoteDrawing;
