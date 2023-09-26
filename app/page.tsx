cd7dd37b07026fcdd0d14e75773f659bf76cfe19
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-09-26 13:51:58 +0200

    page ttret instruccions

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,11 @@ const HomePage = () => {
   return (
     <>
       <h1 className="text-4xl mb-4 text-center">¡Bienvenido al juego!</h1>
-      <p>TODO:pequeñísimas instrucciones</p>
+      {/* <p>TODO:pequeñísimas instrucciones</p> */}
       <div className="mt-4">
         <Link href="/create">
           <Button text="crea una sala"/>
         </Link>
-
-
       </div>
     </>
   );
