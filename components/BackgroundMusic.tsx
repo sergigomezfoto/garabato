4e7906b66aa433f90d10cfb166e83d472a3f43fd
Oriol Rocabert <oriol.rocabert.valverde@gmail.com>
2023-09-27 20:52:10 +0200

    Fixed music component

--- components/BackgroundMusic.tsx
+++ components/BackgroundMusic.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 
@@ -9,28 +9,30 @@ type BackgroundMusicProps = {
 };
 
 const BackgroundMusic: React.FC<BackgroundMusicProps> = ({ audioFile }) => {
-	const [isMuted, setIsMuted] = useState(false);
+	const [isMuted, setIsMuted] = useState(true);
 	const audioRef = useRef<HTMLAudioElement | null>(null);
 
-	const toggleMute = () => {
+	useEffect(() => {
 		if (audioRef.current) {
-			audioRef.current.muted = !isMuted;
-			setIsMuted(!isMuted);
+			if (isMuted) {
+				audioRef.current.pause();
+			} else {
+				audioRef.current.play();
+			}
 		}
+	}, [isMuted]);
+
+	const toggleMute = () => {
+		setIsMuted(!isMuted);
 	};
 
 	return (
 		<div className="music-control">
-			<audio
-				ref={audioRef}
-				src={audioFile}
-				loop
-				autoPlay={!isMuted}
-				muted={isMuted}
-			/>
+			<audio ref={audioRef} src={audioFile} loop muted={isMuted} />
 			<button className="mute-button" onClick={toggleMute}>
 				<FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} />
 			</button>
+			{/* <p>{`muuuuteds${isMuted}`}</p> */}
 		</div>
 	);
 };
