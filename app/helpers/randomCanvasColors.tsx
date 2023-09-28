17ca2ebc1b8094d50e89d9483899a5ba779135f2
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-09-28 17:09:12 +0200

    canvi en randomCanvasColors

--- app/helpers/randomCanvasColors.tsx
+++ app/helpers/randomCanvasColors.tsx
@@ -4,22 +4,15 @@ interface RGBColor {
     b: number;
 }
 
+const MIN_LUMINANCE = 100;
+
 const getRandomInt = (min: number, max: number): number => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
 const calculateLuminance = ({ r, g, b }: RGBColor): number => {
     return 0.2126 * r + 0.7152 * g + 0.0722 * b;
-};
-
-const getColorDistance = (color1: RGBColor, color2: RGBColor): number => {
-    return Math.sqrt(
-        Math.pow(color1.r - color2.r, 2) +
-        Math.pow(color1.g - color2.g, 2) +
-        Math.pow(color1.b - color2.b, 2)
-    );
 };
 
 const generateColor = (): RGBColor => {
@@ -29,23 +22,47 @@ const generateColor = (): RGBColor => {
         b: getRandomInt(0, 255),
     };
 };
+const generateColor2 = (color1: RGBColor): RGBColor => {
+    // Intercanvia els components RGB de color1 com desitges
+    const color2: RGBColor = {
+        r: color1.g,
+        g: color1.b,
+        b: color1.r,
+    };
+    return color2;
+};
 
-const COLORDISTANCE=100;
-const COLORLUMINANCE=100;
+const adjustColorBrightness = (color: RGBColor, targetLuminance: number): RGBColor => {
+    const currentLuminance = calculateLuminance(color);
+        // Si el color és massa fosc, augmenta la lluminositat
+        const luminanceDifference = targetLuminance - currentLuminance;
+        const scaleFactor = (currentLuminance + luminanceDifference) / currentLuminance;
+        // Augmenta els components RGB proporcionalment
+        const adjustedColor: RGBColor = {
+            r: Math.min(255, color.r * scaleFactor),
+            g: Math.min(255, color.g * scaleFactor),
+            b: Math.min(255, color.b * scaleFactor),
+        };
+        return adjustedColor;
+    
+
+};
 
 const randomCanvasColors = (): [string, string] => {
+    // Genera color1
     let color1 = generateColor();
 
-    while (calculateLuminance(color1) > 180) {
+    while (calculateLuminance(color1) > MIN_LUMINANCE) {
+        // console.log('a');    
         color1 = generateColor();
     }
 
-    let color2 = generateColor();
-    while (
-        getColorDistance(color1, color2) > COLORDISTANCE ||
-        calculateLuminance(color2) > COLORLUMINANCE
-    ) {
-        color2 = generateColor();
+    // Ajusta color2 per garantir que sigui més clar si color1 és massa fosc
+    const targetLuminance = MIN_LUMINANCE + 30; // Aquí pots ajustar el valor desitjat
+    let color2 = generateColor2(color1);
+    if (calculateLuminance(color2) < targetLuminance) {
+        console.log('entro aquí');
+        color2 = adjustColorBrightness(color2, targetLuminance);
     }
 
     return [
@@ -53,8 +70,6 @@ const randomCanvasColors = (): [string, string] => {
         `rgb(${color2.r}, ${color2.g}, ${color2.b})`,
     ];
 };
-
-
 //const [color1, color2] = randomCanvasColors();
 
 export default randomCanvasColors;
